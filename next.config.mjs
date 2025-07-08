ad0518cdc71d5a70f459e873e050f6cf9fb353da
Rababb Pannu <rababb.s.pannu@gmail.com>
2025-07-08 16:48:59 -0400

    trying to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio',
+  trailingSlash: true,
+
   eslint: {
     ignoreDuringBuilds: true,
   },
