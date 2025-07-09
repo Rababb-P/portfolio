bc85c9df2e18393993494decddad47a1f494532a
Rababb Pannu <rababb.s.pannu@gmail.com>
2025-07-08 23:28:29 -0400

    remove slash

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
   distDir: 'dist',
   basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
+  assetPrefix: '/portfolio',
   trailingSlash: true,
 
   eslint: {
